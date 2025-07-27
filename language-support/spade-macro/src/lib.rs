18e51f0189f2718536d99d6b62f78c7f20d195fa
Aubrey Taylor <aubrey@hall.ly>
2025-07-27 09:52:17 -0600

    feat(spade): Handle `bool` and `clock` type

--- language-support/spade-macro/src/lib.rs
+++ language-support/spade-macro/src/lib.rs
@@ -234,6 +234,9 @@ fn spade_simple_type_width(type_spec: &spade_ast::TypeSpec) -> usize {
                     }
                     get_constant(&args.as_ref().unwrap().inner[0])
                 }
+                "bool" | "clock" => {
+                    1
+                }
                 _ => panic!("I DONT WANT TO WRITE ERROR MESSAGES"),
             }
         }
